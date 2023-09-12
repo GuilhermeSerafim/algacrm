80ea7342b17f4faeeb7c4cbec2d53787eba2b060
GuilhermeSerafim <guilerstudies@gmail.com>
2023-09-12 09:58:11 -0300

    Criando interface com JpaRepository para pegar os metodos principais e utilizar em tempo de execução na ClienteController

--- src/main/java/com/algaworks/crm/controller/ClienteController.java
+++ src/main/java/com/algaworks/crm/controller/ClienteController.java
@@ -1,6 +1,8 @@
 package com.algaworks.crm.controller;
 
 import com.algaworks.crm.model.Cliente;
+import com.algaworks.crm.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,11 @@ import java.util.List;
 @RequestMapping("/clientes")
 public class ClienteController {
 
-    @GetMapping
-    public List<Cliente> Listar() {
+    @Autowired //Adicionando instância
+    private ClienteRepository clienteRepository;
 
+    @GetMapping
+    public List<Cliente> listar() {
+        return clienteRepository.findAll();
     }
 }
