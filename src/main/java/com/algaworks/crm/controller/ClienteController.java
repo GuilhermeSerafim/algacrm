8eb5443262a7da0351afc9804923d8ce13be35e6
GuilhermeSerafim <guilerstudies@gmail.com>
2023-09-12 10:25:18 -0300

    Criando metodo post com o response 201, retornando um objeto json que foi convertido para Cliente cliente, e salvando no repository

--- src/main/java/com/algaworks/crm/controller/ClienteController.java
+++ src/main/java/com/algaworks/crm/controller/ClienteController.java
@@ -3,9 +3,8 @@ package com.algaworks.crm.controller;
 import com.algaworks.crm.model.Cliente;
 import com.algaworks.crm.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +15,14 @@ public class ClienteController {
     @Autowired //Adicionando instância
     private ClienteRepository clienteRepository;
 
-    @GetMapping
+    @GetMapping //Quando chega uma requisição get a raiz do projeto, cai nesse método
     public List<Cliente> listar() {
         return clienteRepository.findAll();
+    }
+
+    @PostMapping //Quando chega uma requisição post a raiz do projeto, cai nesse método
+    @ResponseStatus(HttpStatus.CREATED) //201 - Created | Se não vai vir um status 200 ...
+    public Cliente adicionar(@RequestBody Cliente cliente) { //Essa anotação faz com que o corpo da requisição que é um objeto JSON seja converido para um objeto JAVA Cliente cliente
+        return clienteRepository.save(cliente);
     }
 }
