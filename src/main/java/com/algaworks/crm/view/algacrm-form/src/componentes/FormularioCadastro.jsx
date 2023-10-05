8fb906fd5bf19c06490cf53ada2d474d9844c4a8
GuilhermeSerafim <guilerstudies@gmail.com>
2023-10-05 11:14:44 -0300

    Criando validação simples de 11 digitos para CPF, e passando como prop para o formulario

--- src/main/java/com/algaworks/crm/view/algacrm-form/src/componentes/FormularioCadastro.jsx
+++ src/main/java/com/algaworks/crm/view/algacrm-form/src/componentes/FormularioCadastro.jsx
@@ -2,18 +2,19 @@ import { Button, FormControlLabel, Switch, TextField } from "@mui/material";
 import React, { useState } from "react";
 
 //O retorno de uma function componente tem que ser minha arvore de renderização
-function FormularioCadastro({aoEnviar}) {
+function FormularioCadastro({ aoEnviar, validarCPF }) {
 	const [nome, setNome] = useState("");
 	const [sobrenome, setSobrenome] = useState("");
 	const [cpf, setCpf] = useState("");
 	const [promocoes, setPromocoes] = useState(true);
 	const [novidades, setNovidades] = useState(true);
+	const [erros, setErros] = useState({ cpf: { valido: true, texto: "" } });
 
 	return (
 		<form
 			onSubmit={(event) => {
 				event.preventDefault();
-				aoEnviar({nome, sobrenome, cpf, promocoes, novidades});
+				aoEnviar({ nome, sobrenome, cpf, promocoes, novidades });
 			}}
 		>
 			<TextField
@@ -43,6 +44,12 @@ function FormularioCadastro({aoEnviar}) {
 				onChange={(event) => {
 					setCpf(event.target.value);
 				}}
+				onBlur={(event) => {
+					const ehValido = validarCPF(event.target.value);
+                    setErros({cpf:ehValido});
+				}}
+				error={!erros.cpf.valido}
+				helperText={erros.cpf.texto}
 				id="cpf"
 				label="CPF"
 				variant="outlined"
